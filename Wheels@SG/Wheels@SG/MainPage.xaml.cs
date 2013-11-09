1b865e2c08666b5b598178f7e601edcb8d07a26e
hzhiguang <rune199@hotmail.com>
2013-11-09 11:10:17 +0800

    ascasc

---
+++ Wheels@SG/Wheels@SG/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Wheels_SG
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Login.xaml", UriKind.Relative));
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Register.xaml", UriKind.Relative));
+        }
+    }
+}
