a84337cb433dbad55f8dd55ea5971a21abcb5ecb
hzhiguang <rune199@hotmail.com>
2013-11-09 12:10:24 +0800

    ascac

---
+++ Wheels@SG/Wheels@SG/AppPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Wheels_SG
+{
+    public partial class AppPage : PhoneApplicationPage
+    {
+        public AppPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            esriMap.Zoom(2);
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            esriMap.Zoom(0.5);
+        }
+    }
+}
